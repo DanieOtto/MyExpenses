ddb6dbcbaaf78c81f4cfe442e7d7d91a4d20c759
Danie Otto <danieotto@live.com>
2026-02-24 22:44:29 +0200

    Updated app folder structure. Added additional pages.

--- MyExpenses/app/(tabs)/_layout.tsx
+++ MyExpenses/app/(tabs)/_layout.tsx
@@ -1,7 +1,30 @@
 import { Tabs } from "expo-router";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function RootLayout() {
-
-    return <Tabs />
+  return (
+    <Tabs initialRouteName="trends">
+      <Tabs.Screen
+        name="expenses"
+        options={{
+          title: 'Expenses',
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="list" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="income"
+        options={{
+          title: 'Income',
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="attach-money" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="trends"
+        options={{
+          title: 'Trends',
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="trending-up" size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
-
