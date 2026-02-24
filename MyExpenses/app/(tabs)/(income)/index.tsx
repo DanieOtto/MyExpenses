4df8b8758ba91779d4fc2f6100fae6046666b2dd
Danie Otto <danieotto@live.com>
2026-02-24 22:45:09 +0200

    Updated files.

--- MyExpenses/app/(tabs)/(income)/index.tsx
+++ MyExpenses/app/(tabs)/(income)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
             alignItems: "center",
         }}
         >
-            <Text>Income</Text>
+            <Text>Income screen</Text>
         </View>
     )
 }
--- MyExpenses/app/(tabs)/trends.tsx
+++ MyExpenses/app/(tabs)/trends.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Dashboard</Text>
+      <Text>Trends</Text>
     </View>
   );
 }
